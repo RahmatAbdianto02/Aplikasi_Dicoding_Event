5415db0d3d7bf393b7482a9a97b80c2c2ec46547
redVELVET <rahmat.abdianto2@gmail.com>
2024-08-13 22:33:45 +0800

    Initial commit

---
+++ app/src/main/java/com/dicoding/aplikasidicodingevent/RecycleView/EventAdapter.kt
@@ -0,0 +1,41 @@
+package com.dicoding.aplikasidicodingevent.ui.home
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.dicoding.aplikasidicodingevent.R
+import com.dicoding.aplikasidicodingevent.RecycleView.ListEventsItem
+
+class EventAdapter(
+    private val context: Context,
+    private val events: List<ListEventsItem>
+) : RecyclerView.Adapter<EventAdapter.EventViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): EventViewHolder {
+        val view = LayoutInflater.from(context)
+            .inflate(R.layout.item_row_image, parent, false)
+        return EventViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: EventViewHolder, position: Int) {
+        val event = events[position]
+        holder.itemText.text = event.name
+
+        Glide.with(context)
+            .load(event.mediaCover)
+            .placeholder(R.drawable.baseline_image_24) // Placeholder image
+            .into(holder.itemImage)
+    }
+
+    override fun getItemCount(): Int = events.size
+
+    inner class EventViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val itemImage: ImageView = itemView.findViewById(R.id.item_Image)
+        val itemText: TextView = itemView.findViewById(R.id.item_Text)
+    }
+}
